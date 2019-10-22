1ae7c1f36650195a40baacd387057d08a423930c
songyuankun <songyuankun@huohua.cn>
2019-10-22 20:03:38 +0800

    订阅服务

--- bin/get_from_subscribe.py
+++ bin/get_from_subscribe.py
@@ -8,9 +8,9 @@ from bin.ParseSsr import cleanupBase64
 def get_from_subscribe():
     properties = get_ssr_properties.get_properties()
     to_test_url = []
-    for url in properties['serverSubscribes']:
+    for serverSubscribe in properties['serverSubscribes']:
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
-        f = request.Request(url, headers=headers)
+        f = request.Request(serverSubscribe['URL'], headers=headers)
         ssr_subscribe = request.urlopen(f).read().decode('utf-8')  # 获取ssr订阅链接中数据
         ssr_subscribe_decode = base64.urlsafe_b64decode(cleanupBase64(ssr_subscribe)).decode('utf-8')
         to_test_url.extend(ssr_subscribe_decode.split('\n'))
