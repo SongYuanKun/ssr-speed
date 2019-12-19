cdf497a5321eacd9966b9d6c392750c8cf2f2016
songyuankun <songyuankun@huohua.cn>
2019-12-19 15:37:04 +0800

    ``

--- foo/main.py
+++ foo/main.py
@@ -41,18 +41,18 @@ class ThreadCrawl(threading.Thread):
 
 
 if __name__ == '__main__':
-    retest_configs()
+    # retest_configs()
 
-    # my_chrome_driver.run_chrome()
+    my_chrome_driver.run_chrome()
     to_test_list = []
-    to_test_list.extend(subscribe_crawler.get_from_subscribe())
-    # to_test_list.extend(ssr_crawler.get_from_ssr_share())
-    # to_test_list.extend(ssr_crawler.get_from_lncn())
-    # to_test_list.extend(ssr_crawler.get_from_SSRSUB())
+    # to_test_list.extend(subscribe_crawler.get_from_subscribe())
+    to_test_list.extend(ssr_crawler.get_from_ssr_share())
+    to_test_list.extend(ssr_crawler.get_from_lncn())
+    to_test_list.extend(ssr_crawler.get_from_SSRSUB())
     # to_test_list.extend(ssr_crawler.get_from_youneed1())
     # to_test_list.extend(ssr_crawler.get_from_youneed2())
     print('to_test_urls=', to_test_list)
-    # my_chrome_driver.close_chrome()
+    my_chrome_driver.close_chrome()
 
     properties = ssr_properties.get_properties()
     configs = properties['configs']
